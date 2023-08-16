12d03ea4193a99180f0dff82b4fdc7a579bfc7c4
Melany Sangucho <117743859+SanguchoMela@users.noreply.github.com>
2023-08-15 21:54:46 -0500

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,24 @@
+var typed = new Typed('#p1', {
+    strings: ['I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth. '],
+    typeSpeed: 80,
+    });
+
+var typed = new Typed('#p2', {
+    strings: [' Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy. '],
+    typeSpeed: 80,
+    });
+
+var typed = new Typed('#p3', {
+    strings: ['An overall wonderful and rewarding experience.'],
+    typeSpeed: 80,
+    });
+
+var typed = new Typed('#p4', {
+    strings: ['The team was very supportive and kept me motivated.'],
+    typeSpeed: 80,
+    });    
+    
+var typed = new Typed('#p5', {
+    strings: ['Such a life-changing experience. Highly recommended! '],
+    typeSpeed: 80,
+    });   
